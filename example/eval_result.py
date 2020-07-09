573fafe01c719165113121ee2d4e5ac0528e985b
loopyme <peter@mail.loopy.tech>
2020-07-09 22:08:32 +0800

    [Doc] Add more detailed info about test result

--- example/eval_result.py
+++ example/eval_result.py
@@ -16,7 +16,8 @@ class EvalResult:
         self.graph_size = len(tuple(node for node in graph._normal_node_map.values() if
                                     node.type not in set(SMSGraph.non_normal_node_type[:1])))
         self.num_subgraph = len(subgraph_deque)
-        self.subgraph_size = sum(len(g.nodes[0]) for g in subgraph_deque)
+        self.subgraph_size = tuple(len(g.nodes[0]) for g in subgraph_deque)
+        self.subgraph_count = tuple(len(g.nodes) for g in subgraph_deque)
         self.new_graph_size = self.graph_size - len(
             tuple(node for subgraph in subgraph_deque for instance in subgraph.nodes for node in instance))
 
@@ -32,7 +33,7 @@ class EvalResult:
              - dl(g|s)): total size of the graph which remove all the detected subgraphs
         """
 
-        return self.graph_size / (self.subgraph_size + self.new_graph_size)
+        return self.graph_size / (sum(self.subgraph_size) + self.new_graph_size)
 
     @property
     def minimum_description_length(self):
@@ -40,11 +41,13 @@ class EvalResult:
 
     @property
     def reduce_ratio(self) -> float:
-        return (self.graph_size - self.subgraph_size - self.new_graph_size) / self.graph_size
+        return (self.graph_size - sum(self.subgraph_size) - self.new_graph_size) / self.graph_size
 
     def __repr__(self):
         return f"+++++++++Eval Result+++++++++\n" \
                f"Graph Size = {self.graph_size}\n" \
                f"No. subgraph = {self.num_subgraph}\n" \
+               f"No. Subgraph instance= {self.subgraph_count}\n" \
+               f"Subgraph size= {self.subgraph_size}\n" \
                f"MDL = {self.MDL:.2f}\n" \
                f"Reduce ratio = {self.reduce_ratio:.2f}"
